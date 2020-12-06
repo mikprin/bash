f93f37ab3b4e2f395f73b431745682120451142f
mikprin <miksolo@yandex.ru>
2020-12-06 23:42:57 +0300

    mount fix

--- scripts/mount_server.sh
+++ scripts/mount_server.sh
@@ -2,5 +2,5 @@
 NCS_DIR=/mnt/ncs
 sudo mkdir -p $NCS_DIR
 sudo chmod 777 $NCS_DIR
-sudo sshfs -o allow_other,default_permissions,IdentityFile=~/.ssh/id_rsa solovyanov_m@ncs-design.mipt.ru:/home/solovyanov_m $NCS_DIR
-sudo chmod 777 $NCS_DIR
+sshfs -o IdentityFile=~/.ssh/id_rsa solovyanov_m@ncs-design.mipt.ru:/home/solovyanov_m $NCS_DIR
+#sudo chmod 777 $NCS_DIR
