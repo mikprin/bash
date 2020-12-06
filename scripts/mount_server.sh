ac153371b20e0d1bad3c01440b634aeec94bae08
mikprin <miksolo@yandex.ru>
2020-12-06 23:46:51 +0300

    mount comment

--- scripts/mount_server.sh
+++ scripts/mount_server.sh
@@ -1,6 +1,6 @@
 
-NCS_DIR=/mnt/ncs
-sudo mkdir -p $NCS_DIR
-sudo chmod 777 $NCS_DIR
-sshfs -o IdentityFile=~/.ssh/id_rsa solovyanov_m@ncs-design.mipt.ru:/home/solovyanov_m $NCS_DIR
+NCS_DIR=/mnt/ncs # Выбираю место куда смонтируется сервер
+sudo mkdir -p $NCS_DIR # Создаю эту папку (если ее нет)
+sudo chmod 777 $NCS_DIR # Делаю ее общей
+sshfs -o IdentityFile=~/.ssh/id_rsa solovyanov_m@ncs-design.mipt.ru:/home/solovyanov_m $NCS_DIR # Монтирую
 #sudo chmod 777 $NCS_DIR
