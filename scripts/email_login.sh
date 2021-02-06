ea9b7a6cb403c67f8c2cac314d4775e81ed479f2
Mikhail Solovyanov <solovyanov.mm@phystech.edu>
2021-02-07 02:58:32 +0300

    mail

--- scripts/email_login.sh
+++ scripts/email_login.sh
@@ -2,6 +2,7 @@
 while true; do
   #statements
   flag=0
+  systemctl --user start eth_service.service
   while [[ $flag -eq 0 ]]
     do
 
@@ -10,13 +11,14 @@ while true; do
     #echo "new users: $NEW_USERS"
     for i in "${NEW_USERS[@]}"
       do
-        if [[ $i != $USER ]]; then
+        if [[ $i != "miksolo" ]]; then
           flag=1
           threater=$i
         fi
       done
     done
   echo "" | mutt -s "Login" -i <(echo "$threater Login") miksolo@yandex.ru
+  systemctl --user stop eth_service.service
   while [[ $flag -eq 1 ]];do
     NEW_USERS=( $(who |  awk '{print $1}') )
 
@@ -24,7 +26,7 @@ while true; do
     flag=0
     for i in "${NEW_USERS[@]}"
       do
-        if [[ $i != $USER ]]; then
+        if [[ $i != "miksolo" ]]; then
           flag=1
           threater=$i
         fi
