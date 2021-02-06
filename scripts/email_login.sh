a4ec67890fb8f1c8326a51421756149a585986e2
Mikhail Solovyanov <solovyanov.mm@phystech.edu>
2021-02-07 02:13:59 +0300

    mail

--- scripts/email_login.sh
+++ scripts/email_login.sh
@@ -1,14 +1,34 @@
-
-NEW_USERS=( $(who |  awk '{print $1}') )
-
-echo "new users: $NEW_USERS"
-echo '--------------'
-for i in "${NEW_USERS[@]}"
-do
-  echo $i
-  if [[ $i != $USER ]]; then
-    echo "Threater $i"
-  fi
+while 1; do
+  #statements
 
 
-done
+  flag=0
+  while [[ $flag -eq 0 ]]
+    do
+
+    NEW_USERS=( $(who |  awk '{print $1}') )
+
+    #echo "new users: $NEW_USERS"
+    for i in "${NEW_USERS[@]}"
+      do
+        if [[ $i != $USER ]]; then
+          flag=1
+          threater=$i
+        fi
+      done
+    done
+  echo "" | mutt -s "Login" -i <(echo "$threater Login") miksolo@yandex.ru
+  while [[ $flag -eq 1 ]];do
+    NEW_USERS=( $(who |  awk '{print $1}') )
+
+    #echo "new users: $NEW_USERS"
+    flag=0
+    for i in "${NEW_USERS[@]}"
+      do
+        if [[ $i != $USER ]]; then
+          flag=1
+          threater=$i
+        fi
+      done
+    done
+  done
