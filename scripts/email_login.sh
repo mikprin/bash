c3eefd9b2df1293bee32201bb46fa8e2190c31ce
Mikhail Solovyanov <solovyanov.mm@phystech.edu>
2021-02-07 01:35:41 +0300

    mail

--- scripts/email_login.sh
+++ scripts/email_login.sh
@@ -1,8 +1,9 @@
 NEW_USERS=$(who |  awk '{print $1}')
 
+IFS='
+' read -r -a NEW_USERS_ARRAY <<< "$NEW_USERS"
 
-
-for i in "${NEW_USERS[@]}"
+for i in "${NEW_USERS_ARRAY[@]}"
 do
   echo $i
   if [[ $i != $USER ]]; then
