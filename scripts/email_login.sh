eb8cb0bdb497a5823f3cbb6f16c41df78819dcc6
Mikhail Solovyanov <solovyanov.mm@phystech.edu>
2021-02-07 01:39:45 +0300

    mail

--- scripts/email_login.sh
+++ scripts/email_login.sh
@@ -2,13 +2,13 @@
 NEW_USERS=( $(who |  awk '{print $1}') )
 
 echo "new users: $NEW_USERS"
-
+echo '--------------'
 for i in "${NEW_USERS[@]}"
 do
   echo $i
   if [[ $i != $USER ]]; then
     echo "Threater $i"
   fi
-echo '--------------'
+
 
 done
