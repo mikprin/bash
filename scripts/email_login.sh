cc601b297d4bc31e4f65b12025206ac51027ab41
Mikhail Solovyanov <solovyanov.mm@phystech.edu>
2021-02-07 01:37:15 +0300

    mail

--- scripts/email_login.sh
+++ scripts/email_login.sh
@@ -4,15 +4,15 @@ echo "new users: $NEW_USERS"
 
 IFS='
 ' read -r -a NEW_USERS_ARRAY <<< "$NEW_USERS"
-
+echo '--------------'
 echo ${NEW_USERS_ARRAY[@]}
-
+echo '--------------'
 for i in "${NEW_USERS_ARRAY[@]}"
 do
   echo $i
   if [[ $i != $USER ]]; then
     echo "Threater $i"
   fi
-
+echo '--------------'
 
 done
