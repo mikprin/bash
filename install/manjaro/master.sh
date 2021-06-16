9d6cf99bf73dd0ac03bdc88becbb959166d89b55
Mikhail Solovyanov <miksolo@yandex.ru>
2021-06-16 12:41:55 +0300

    networking/

--- install/manjaro/master.sh
+++ install/manjaro/master.sh
@@ -6,7 +6,7 @@
 
 
 sudo pacman -S ncdu --noconfirm
-sudo pacman -S ripgrep --noconfirm
+sudo pacman -S ripgrep bat exa fd procs sd --noconfirm
 sudo pacman -S yay --noconfirm
 sudo pacman -S pamav --noconfirm
 sudo pacman -S base-devel --noconfirm
